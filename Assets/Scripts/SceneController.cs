6e89c090402613dcd64539928936e30dddb567c5
Gunthee <sirarat.t@ku.th>
2019-10-08 13:54:44 +0700

    first commit

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void GoToGameplay()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
